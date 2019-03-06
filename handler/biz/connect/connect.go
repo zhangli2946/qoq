eef48510f6d4b8464ae3b93825ad8ffa5ee8a411
zhangli#2946 <294638057@qq.com>
2019-03-06 15:28:35 +0800

    action: connect handler notice: XD

---
+++ handler/biz/connect/connect.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"qoq/actor/emq"
+	"time"
+
+	client "github.com/eclipse/paho.mqtt.golang"
+)
+
+type input struct {
+	C int `json:"cnt"`
+	B int `json:"cps"`
+}
+
+// Connect handler
+func Connect(i *emq.Emq, cmd json.RawMessage) (err error) {
+	var d input
+	err = json.Unmarshal(cmd, &d)
+	if err != nil {
+		return
+	}
+	if d.B == 0 {
+
+	} else {
+		cnt := d.B
+		for ; cnt > 0; cnt-- {
+			for step := d.C / d.B; step > 0; step-- {
+				go func(ctx context.Context, c int, s int) {
+					var tk client.Token
+					cli := client.NewClient(client.NewClientOptions().SetClientID(fmt.Sprintf("%d@%d", c, s)))
+
+					if cli.Connect(); tk.Wait() && tk.Error() != nil {
+						fmt.Println(tk.Error(), "@", c, "@", s)
+					}
+					time.Sleep(100 * time.Second)
+					cli.Disconnect(250)
+				}(i.Ctx, cnt, step)
+			}
+		}
+	}
+	return nil
+}
+
+// Gen func
+func Gen() (string, func(*emq.Emq, json.RawMessage) error) {
+	return "connect", Connect
+}
+
+func main() {
+}
